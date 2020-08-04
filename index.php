a9709a0f03c54bece2fb1ffba22f43bea345e2c2
Marie Gandrille <mgandrille@yahoo.fr>
2020-08-04 20:55:13 +0200

    Structure de la page mise en place

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Style Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--Icones-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel='stylesheet' href='style.css'>
+
+    <title>Ingrid Lucas Osthéopathe</title>
+</head>
+
+    <body>
+        <header id="header" class="container-fluid bg-light">
+            <p>Ingrid Lucas - Osthéopathe D.O. / 24 bis rue de Chassagne - 69360 Ternay</p>
+            <p>
+                Consultations sur rendez-vous
+                <a href="tel:+33627584026">
+                    <i class="fas fa-phone-alt"></i>
+                    06 27 58 40 26
+                </a>
+                ou en ligne sur
+                <a href="https://www.doctolib.fr/osteopathe/communay/ingrid-lucas" target="_blank" rel="noopener noreferrer">Doctolib</a>
+            </p>
+
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Quand consulter</li>
+                    <li>Séance</li>
+                    <li>Rendez-vous</li>
+                    <li>Informations pratiques</li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <div id="presentation">
+                <h1>Ingrid Lucas</h1>
+                <h2>Osthéopathe D.O.</h2>
+            </div>
+
+            <article id="cabinet">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum laudantium aut facere ducimus nulla fuga qui dolorum fugiat odio! Excepturi ipsam delectus quas fuga ab laudantium dolor. Qui, a aperiam?</p>
+                <img src="https://www.via.placeholder.com/75" alt="">
+            </article>
+
+            <article id="consultation">
+
+            </article>
+
+            <article id="seance">
+
+            </article>
+
+            <article id="rdv">
+
+            </article>
+
+            <article id="infos">
+
+            </article>
+        </main>
+
+        <footer>
+        </footer>
+
+        <!-- *** SCRIPTS *** -->
+        <!--Bootstrap JS-->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <!-- JavaScript interne -->
+        <script src="scripts/script.js"></script>
+
+    </body>
+</html>

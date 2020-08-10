05b80d10ffc1cf3692f4969c38cfb2c2747b175c
Marie Gandrille <mgandrille@yahoo.fr>
2020-08-10 11:21:06 +0200

    Suite de la mise en place de l'architecture du site

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
 
     <link rel='stylesheet' href='style.css'>
 
-    <title>Ingrid Lucas Osthéopathe</title>
+    <title>Ingrid Lucas Ostéopathe</title>
 </head>
 
     <body>
-        <header id="header" class="container-fluid bg-light">
+        <header id="header" class="container-fluid">
             <div>
-              <p>Ingrid Lucas - Osthéopathe D.O.</p>
+              <p>Ingrid Lucas - Ostéopathe D.O.</p>
               <p>24 bis rue de Chassagne - 69360 Ternay</p>
             </div>
             <div>
@@ -32,55 +32,132 @@
                     06 27 58 40 26
                 </a>
                 ou en ligne sur
-                <a href="https://www.doctolib.fr/osteopathe/communay/ingrid-lucas" target="_blank" rel="noopener noreferrer">Doctolib</a>
+                <a href="https://www.doctolib.fr/osteopathe/communay/ingrid-lucas" target="_blank" rel="">Doctolib</a>
             </div>
 
-            <nav id="navbar">
-                <ul>
-                    <li>Accueil</li>
-                    <li>Quand consulter</li>
-                    <li>Séance</li>
-                    <li>Rendez-vous</li>
-                    <li>Informations pratiques</li>
-                </ul>
+            <nav id="navbar" class="navbar navbar-expand-md navbar-light fixed-top">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+                    <ul>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Accueil</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Quand consulter</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Séance</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Rendez-vous</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Informations pratiques</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
 
         <main>
             <div id="presentation">
                 <h1>Ingrid Lucas</h1>
-                <h2>Osthéopathe D.O.</h2>
+                <h2>Ostéopathe D.O.</h2>
+                <div class="lien-rdv">
+                    <a href="https://www.doctolib.fr/osteopathe/communay/ingrid-lucas" target="_blank">
+                        Prendre rendez-vous en ligne
+                    </a>
+                </div>
             </div>
 
             <article id="cabinet">
+                <h2>Le cabinet</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum laudantium aut facere ducimus nulla fuga qui dolorum fugiat odio! Excepturi ipsam delectus quas fuga ab laudantium dolor. Qui, a aperiam?</p>
-                <img src="https://www.via.placeholder.com/75" alt="">
+                <img src="https://www.via.placeholder.com/75" alt="Ingrid Lucas Ostéopathe">
             </article>
 
             <article id="consultation">
+                <h2>Quand consulter un ostéopathe ?</h2>
                 <div>
                   <img src="" alt="" >
-                  <p>Lorem ipsum</p>
+                  <p>Adultes</p>
                 </div>
                 <div>
                   <img src="" alt="" >
-                  <p>Lorem ipsum</p>
+                  <p>Enfants</p>
                 </div>
                 <div>
                   <img src="" alt="" >
-                  <p>Lorem ipsum</p>
+                  <p>Femmes enceintes</p>
                 </div>
             </article>
 
             <article id="seance">
+                <h2>Comment se déroule une séance ?</h2>
 
+                <div id="etape1">
+                    <h3>Etape 1</h3>
+                    <img src="https://via.placeholder.com/75" alt="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur perferendis impedit a quibusdam ipsum aliquid voluptates. Totam quo facilis error sequi porro voluptates libero vitae modi quae repellendus! Unde, itaque!</p>
+                </div>
+
+                <div id="etape2">
+                    <h3>Etape 2</h3>
+                    <img src="https://via.placeholder.com/75" alt="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur perferendis impedit a quibusdam ipsum aliquid voluptates. Totam quo facilis error sequi porro voluptates libero vitae modi quae repellendus! Unde, itaque!</p>
+                </div>
+
+                <div id="etape3">
+                    <h3>Etape 3</h3>
+                    <img src="https://via.placeholder.com/75" alt="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur perferendis impedit a quibusdam ipsum aliquid voluptates. Totam quo facilis error sequi porro voluptates libero vitae modi quae repellendus! Unde, itaque!</p>
+                </div>
+
+                <div id="etape4">
+                    <h3>Etape 4</h3>
+                    <img src="https://via.placeholder.com/75" alt="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur perferendis impedit a quibusdam ipsum aliquid voluptates. Totam quo facilis error sequi porro voluptates libero vitae modi quae repellendus! Unde, itaque!</p>
+                </div>
             </article>
 
             <article id="rdv">
+                <div>
+                    <h4>Pour prendre rendez-vous</h4>
+                    <p>
+                        Je suis joignable au
+                        <a href="tel:+33627584026">
+                            06 27 58 40 26
+                        </a>
+                    </p>
+
+                    <p>ou directement sur Internet </p>
+                    <div class="lien-rdv">
+                        <a href="https://www.doctolib.fr/osteopathe/communay/ingrid-lucas" target="_blank">
+                            Prendre rendez-vous en ligne
+                        </a>
+                    </div>
+
+                </div>
+
+                <div>
+                    <h4>honoraires</h4>
+                    <p>Tarif de la consulation <span class="bold">55 euros</span></p>
+                    <p>Les séances d'ostéopathie ne sont pas remboursées par la sécurité sociale mais le sont par les mutuelles</p>
+                    <p>Je vous délivre une facture à envoyer à votre mutuelle pour le remboursement</p>
+                </div>
 
             </article>
 
             <article id="infos">
+                <h6>Ingrid Lucas</h6>
+                <h6>Ostéopathe D.O.</h6>
+                <p>24 bis rue de Chassagne <br> 69360 Ternay</p>
+                <a href="tel:+33627584026">
+                    <i class="fas fa-phone-alt"></i>
+                    06 27 58 40 26
+                </a>
 
             </article>
         </main>

377e3b4c8ccc6bf4620cd2f4623ff53a2e8024c0
Marie Gandrille <mgandrille@yahoo.fr>
2020-08-30 13:15:39 +0200

    Ajoute d'un footer simple

--- wp-content/themes/ingridlucas/footer.php
+++ wp-content/themes/ingridlucas/footer.php
@@ -12,19 +12,9 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ingridlucas' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ingridlucas' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ingridlucas' ), 'ingridlucas', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="container-fluid">
+			&copy; <?= date('Y') ?> Ingrid Lucas Ostéopathe D.O.
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
